c4ff899a4c7a4adfa6b1fc534ce25989e8fc9c79
Turaconokiki <turaconjoy@gmail.com>
2022-08-13 12:58:59 +0100

    note keeper app

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      
+      {notes.map(details => 
+       <Note
+       key={details.key}
+       title={details.title}
+       content={details.content}
+
+       />)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
